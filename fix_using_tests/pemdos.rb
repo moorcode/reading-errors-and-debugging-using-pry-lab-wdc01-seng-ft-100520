2ca2faab500d607ed28862c49995cdc3fea1b6fe
Christina Poole <codenamedchris@gmail.com>
2020-09-15 19:20:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    string[0].times10 + string
+    10 * "s" + string
+    binding.pry
   else
     string
   end
